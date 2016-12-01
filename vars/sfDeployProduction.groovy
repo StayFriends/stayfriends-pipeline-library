2f1cb8f1edf7529bb53bfdd097697bffa3a62811
Adrian Halamoda <adrian.halamoda@stayfriends.com>
2016-12-01 16:46:43 +0100

    dev deploy production

--- vars/sfDeployProduction.groovy
+++ vars/sfDeployProduction.groovy
@@ -10,11 +10,24 @@ def call(body) {
 
     def utils = new io.fabric8.Utils()
 
+    //sfCheckout {}
+    //def buildVersion = sfMavenBuildRelease {}
+    //sfMavenIntegrationTest {}
+    //sfDeployStaging {}
+
+    // resolve project name
+    // get version / input / last version
+    // get proper kubernetes rc from nexus staging repo
+    //  config.version = "0.${env.BUILD_NUMBER}"
+
+    // sfApprove {
+    //   version = buildVersion
+    // }
+
+    // deploy kubernetes rc
+
     if ( !config.name ) {
     	config.name = env.JOB_NAME
-    }
-    if ( !config.group ) {
-    	config.group = "webapp"
     }
     if ( !config.version ) {
     	config.version = "0.${env.BUILD_NUMBER}"
@@ -23,32 +36,35 @@ def call(body) {
 		// default full image name including registry
 		config.image = "${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${utils.getNamespace()}/${config.name}:${config.version}"
     }
+
     echo "deploy config = " + config
 
-    //container(name: 'client') {
-	stage 'Rollout Production'
-		def envNamespace = utils.environmentNamespace('production')
-		echo "deploying to environment: " + envNamespace
+ //    //container(name: 'client') {
+	// stage 'Rollout Production'
+	// 	def envNamespace = utils.environmentNamespace('production')
+	// 	echo "deploying to environment: " + envNamespace
 
-		// this file is read as default, as it is produced by maven plugin f-m-p
-		rcName = "target/classes/META-INF/fabric8/kubernetes.yml"
-		rc = ""
-		if ( fileExists(rcName) ) {
-			rc = readFile file: rcName
-		} else {
-			// generate default resources
-			rc = sfKubernetesResourceWebapp {
-				name = config.name
-				group = config.group
-				version = config.version
-				port = 80
-				image = config.image
-				icon = "https://cdn.rawgit.com/fabric8io/fabric8/dc05040/website/src/images/logos/nodejs.svg"
-			}
-		}
+	// 	// get rc from nexus repo
 
-		echo "applying kubernetes rc: " + rc
-		kubernetesApply(file: rc, environment: envNamespace)
-		//sh "kubectl apply -f target/classes/META-INF/fabric8/kubernetes.yml"
-	//}
+	// 	// this file is read as default, as it is produced by maven plugin f-m-p
+	// 	rcName = "target/classes/META-INF/fabric8/kubernetes.yml"
+	// 	rc = ""
+	// 	if ( fileExists(rcName) ) {
+	// 		rc = readFile file: rcName
+	// 	} else {
+	// 		// generate default resources
+	// 		rc = sfKubernetesResourceWebapp {
+	// 			name = config.name
+	// 			group = config.group
+	// 			version = config.version
+	// 			port = 80
+	// 			image = config.image
+	// 			icon = "https://cdn.rawgit.com/fabric8io/fabric8/dc05040/website/src/images/logos/nodejs.svg"
+	// 		}
+	// 	}
+
+	// 	echo "applying kubernetes rc: " + rc
+	// 	kubernetesApply(file: rc, environment: envNamespace)
+	// 	//sh "kubectl apply -f target/classes/META-INF/fabric8/kubernetes.yml"
+	// //}
 }
