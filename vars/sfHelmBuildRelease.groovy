12db7ab7a7b237aeed571e62b8d3ce93a07e2440
Adrian Halamoda <adrian.halamoda@stayfriends.com>
2017-10-19 14:50:22 +0200

    publish helm chart

--- vars/sfHelmBuildRelease.groovy
+++ vars/sfHelmBuildRelease.groovy
@@ -30,17 +30,26 @@ def call(body) {
                 // package chart
                 sh "helm package --version ${config.version} ${chartDir}"
                 def chartPackage = "${config.name}-${config.version}.tgz"
-                sh "mkdir -p target/helm"
-                sh "mv ${chartPackage} target/helm"
-                chartPackage = "target/helm/${chartPackage}"
 
-                def s3Endpoint = "http://helmrepo-minio-svc"
-                def helmRepoPath = "testing"
-                sh "aws --endpoint-url ${s3Endpoint} s3 cp ${chartPackage} s3://${helmRepoPath}"
+                def helmDir = "target/helm"
+                sh "mkdir -p ${helmDir}"
+                sh "mv ${chartPackage} ${helmDir}"
+                chartPackage = "${helmDir}/${chartPackage}"
 
                 // publish helm chart to helm repo
+                def s3Endpoint = "http://helmrepo-minio-svc"
+                def helmRepoPath = "testing"
+                def helmRepoUrl = "${s3Endpoint}/${helmRepoPath}"
+                // download index
+                sh "aws --endpoint-url ${s3Endpoint} s3 cp s3://${helmRepoPath}/index.yaml ${helmDir}"
+                // update index
+                sh "helm repo index --url ${helmRepoUrl} --merge ${helmDir}/index.yaml ${helmDir}"
+                // upload chart
+                sh "aws --endpoint-url ${s3Endpoint} s3 cp ${chartPackage} s3://${helmRepoPath}"
+                // upload index
+                sh "aws --endpoint-url ${s3Endpoint} s3 cp ${helmDir}/index.yaml s3://${helmRepoPath}"
+
                 //sh "helm repo add sf-testing http://helmrepo-minio-svc.f8.test-kublet-cluster.nbg1.stayfriends.de/testing"
-                // update repo index
 
             }
         }
