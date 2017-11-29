325f34337b72bfe8e73ed3489433657d78d7b1bf
ffeiten <frederik.feiten@stayfriends.com>
2017-11-29 14:46:22 +0100

    script for npm publish

---
+++ vars/sfNodeNpmPublish.groovy
@@ -0,0 +1,23 @@
+#!/usr/bin/groovy
+
+def call(body) {
+
+    container(name: 'ng2-builder') {
+
+        stage 'Dependencies'
+        sh 'npm config list'
+        sh 'npm --loglevel info install'
+
+        if ( fileExists("src") ) {
+            stage 'Test'
+            env.NODE_ENV = "test"
+            sh 'npm run lint'
+            sh 'Xvfb :99 -screen 0 1024x768x16 &'
+            sh 'npm run test'
+        }
+
+        stage 'NPM Publish'
+        sh 'npm config set registry http://???'
+        sh 'npm publish'
+    }
+}
