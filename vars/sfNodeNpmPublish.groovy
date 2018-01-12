2f574f40e07e45d4686070037fb5e7ecf4704b33
Martin Fochler <martin.fochler@stayfriends.com>
2018-01-12 09:13:13 +0100

    remove pod definition from publish script and just copy the config from the mount path

--- vars/sfNodeNpmPublish.groovy
+++ vars/sfNodeNpmPublish.groovy
@@ -6,38 +6,27 @@ def call(body) {
 
     echo "podTemplate label: " + nlabel
 
-    podTemplate(
-        name: nlabel,
-        label: nlabel,
-        serviceAccount: 'jenkins',
-        containers: [
-            [name: 'ng2-builder'],
-        ],
-        volumes: [
-                [$class: 'SecretVolume', mountPath: '/jenkins-config/', secretName: 'jenkins-npm-settings']
-        ]) {
-        node(nlabel) {
+    container(name: 'ng2-builder') {
 
-            stage 'Dependencies'
-            sh 'npm config list'
-            sh 'npm --loglevel info install'
+        stage 'Dependencies'
+        sh 'npm config list'
+        sh 'npm --loglevel info install'
 
-            if ( fileExists("src") ) {
-                stage 'Test'
-                env.NODE_ENV = "test"
-                sh 'npm run lint'
-                sh 'Xvfb :99 -screen 0 1024x768x16 &'
-                sh 'npm run test'
-            }
+        if ( fileExists("src") ) {
+            stage 'Test'
+            env.NODE_ENV = "test"
+            sh 'npm run lint'
+            sh 'Xvfb :99 -screen 0 1024x768x16 &'
+            sh 'npm run test'
+        }
 
-            stage 'Build'
-            sh 'npm run build'
+        stage 'Build'
+        sh 'npm run build'
 
-            if ( fileExists("dist") ) {
-                stage 'NPM Publish'
-                sh 'cp /jenkins-config/* dist/'
-                sh 'cd dist && npm publish'
-            }
+        if ( fileExists("dist") ) {
+            stage 'NPM Publish'
+            sh 'cp /home/jenkins/npm-config/* dist/'
+            sh 'cd dist && npm publish'
         }
     }
 }
