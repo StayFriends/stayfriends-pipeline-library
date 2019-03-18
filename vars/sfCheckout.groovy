ff127a75f3b2f96d8815ce8f5ebb9054ec329c2e
Alexander Sauerbier <alexander.sauerbier@stayfriends.com>
2019-03-18 10:31:26 +0100

    .

--- vars/sfCheckout.groovy
+++ vars/sfCheckout.groovy
@@ -4,7 +4,8 @@ def call(body) {
     stage('Checkout') {
         //git GIT_URL
         // git access configured for root user
-        sh 'chmod -R 600 /root/.ssh' // required for ssh, as long, as it is not supported as option in podTemplate
+        //not allowed anymore sh 'chmod -R 600 /root/.ssh' 
+        // required for ssh, as long, as it is not supported as option in podTemplate
         checkout scm
     }
     
